d36b5179ef20a85905a9f365823239067187e727
AleksandrTsarik <ascwork86@gmail.com>
2021-03-24 12:04:40 +0500

    new change

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
+
 import './index.css';
-// import App from './App';
 import reportWebVitals from './reportWebVitals';
 import booksData from './localStorage';
 import { rerenderEntireTree } from './render'
